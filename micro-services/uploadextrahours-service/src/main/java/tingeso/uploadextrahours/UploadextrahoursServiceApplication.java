53d9e90c9334f57a82c01b31a994cf27f1ac64c4
AlexPacheco <alex.pacheco@usach.cl>
2022-11-14 18:26:48 -0300

    delete config-service

--- micro-services/uploadextrahours-service/src/main/java/tingeso/uploadextrahours/UploadextrahoursServiceApplication.java
+++ micro-services/uploadextrahours-service/src/main/java/tingeso/uploadextrahours/UploadextrahoursServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class UploadextrahoursServiceApplication {
 
     public static void main(String[] args) {
